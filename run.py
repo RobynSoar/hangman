a6956f153d8a6d9d5757fd6918952cc37d0210d1
Robyn Soar <robyn999@hotmail.co.uk>
2024-07-10 12:58:09 +0000

    Fix all linter issues throughout run.py to pass linter

--- run.py
+++ run.py
@@ -1,27 +1,40 @@
-# Imports
-from pyfiglet import figlet_format # Imports pyfiglet to create ASCII art
-from colorama import Fore, Style, init # Imports colorama to colour ASCII  art and reset colour
-import sys # Imports the sys module to exit the system
-import random # Imports the random module to allow function to return random words
-import title # Imports from title.py
-from words import words # Imports from words.py
-from hangman import display_hangman # Imports from hangman.py
+"""
+Hangman Game Script
+
+This script allows users to play the classic Hangman game in the terminal.
+
+Author: Robyn Soar
+Date: July 2024
+"""
+
+import sys  # Imports the sys module to exit the system
+# Imports the random module to allow function to return random words
+import random
+# Imports colorama to colour ASCII  art and reset colour
+from colorama import Fore, init
+import title  # Imports from title.py
+from words import words  # Imports from words.py
+from hangman import display_hangman  # Imports from hangman.py
 
 # Initilizes colorama to reset colour changes
 init(autoreset=True)
+
 
 def print_welcome_message():
     """
     Print a welcome message to the user upon starting Hangman game terminal.
     Provides basic guideline and rules for user.
     """
-    print(title.title) # Import title from title.py with styling
+    print(title.title)  # Import title from title.py with styling
     print("Welcome to Hangman!")
-    print("Hangman is a game about guessing a random word one letter at a time.\n")
-    print("- The word is hidden at the beginning and correct guesses will reveal it.")
+    print("Hangman is a game about guessing a random word one letter at a \
+        time.\n")
+    print("- The word is hidden at the beginning and correct guesses will \
+        reveal it.")
     print("- You can guess individual letters or the whole word!")
     print("- You have 7 wrong guesses before it's Game Over.")
-    print("- You can leave the game at any time by typing 'exit' into the terminal at any time\n")
+    print("- You can leave the game at any time by typing 'exit' into the \
+        terminal at any time\n")
 
 
 def play_question():
@@ -29,8 +42,9 @@ def play_question():
     Validates if the user would like to proceed with game play.
     Checks if user input is valid.
     """
-    while True: # Loops till user agrees to play
-        play = input("Would you like to play? (y/n):\n").lower() # .lower to allow Capitilised Y/N
+    while True:  # Loops till user agrees to play
+        # .lower to allow Capitilised Y/N
+        play = input("Would you like to play? (y/n):\n").lower()
         # User input validation
         if play != "y" and play != "n":
             print(Fore.RED + "Please enter either 'y' for Yes or 'n' for No")
@@ -49,23 +63,29 @@ def get_player_name():
     while True:
         username = input("Excellent! Please enter your name:\n")
         if len(username) <= 1:
-            print(Fore.RED + "Please enter a name of at least two letters! Try again.")
+            print(Fore.RED + "Please enter a name of at least two letters! \
+                Try again.")
         elif not username.isalpha():
-            print(Fore.RED + "Your name should contain only alphabetical characters, try again.")
+            print(Fore.RED + "Your name should contain only alphabetical \
+                characters, try again.")
         else:
-            print(Fore.CYAN + f"Hello, {username}! Nice to meet you, let's play!\n")
+            print(Fore.CYAN + f"Hello, {username}! Nice to meet you, let's \
+                play!\n")
             return username
 
 
 def select_random_word():
     """
-    Selects a word and it's corrponding hint from the words dictionary within words.py.
+    Selects a word and it's corrponding hint from the words dictionary \
+        within words.py.
     Returns words in uppercase and hints capitilized.
     """
     selected_word = random.choice(words)
-    word = selected_word["word"] # Chooses random word from words.py
-    hint = selected_word["hint"] # Gets the chosen word's hint from words.py
-    return word.upper(), hint.capitalize() # Returns words in uppercase and hints capitalised
+    word = selected_word["word"]  # Chooses random word from words.py
+    hint = selected_word["hint"]  # Gets the chosen word's hint from words.py
+    # Returns words in uppercase and hints capitalised
+    return word.upper(), hint.capitalize()
+
 
 # Inspired code
 def play_game(word, hint, username):
@@ -78,7 +98,8 @@ def play_game(word, hint, username):
     guessed_words = []
     tries = 7
 
-    print(Fore.YELLOW + f"You've got 7 wrong guesses before the gallows are full, {username}! You can do this!\n")
+    print(Fore.YELLOW + f"You've got 7 wrong guesses before the gallows are \
+        full, {username}! You can do this!\n")
     print(display_hangman(tries))
     print(word_completion)
     print(f"Hint: {hint}\n")
@@ -87,14 +108,22 @@ def play_game(word, hint, username):
 
     while not guessed and tries > 0:
         guess = input("Enter a letter or try for the whole word: \n")
-        if len(guess) == 1 and guess.isalpha(): # If guess is a single alphabetic letter
+        # If guess is a single alphabetic letter
+        if len(guess) == 1 and guess.isalpha():
             print("You entered a letter")
-        elif len(guess) == len(word) and guess.isalpha(): # If guess is the same length as chosen word and alphabetic
+        # If guess is the same length as chosen word and alphabetic
+        elif len(guess) == len(word) and guess.isalpha():
             print("You entered a full word")
-        elif not (len(guess) == 1 or len(guess) == len(word)) and guess.isalpha(): # If input is alphabetic but more than a single letter but not length of word
-            print(Fore.RED + "Your guess needs to be either a single letter or the same length as the whole word, please try again.")
-        else: # If input contains any numbers or special characters
-            print(Fore.RED + "Please try again with no numbers or special characters!")
+        # If input is alphabetic but more than a letter but not len of word
+        elif (
+            not (len(guess) == 1 or len(guess) == len(word))
+            and guess.isalpha()
+        ):
+            print(Fore.RED + "Your guess needs to be either a single letter \
+                or the same length as the whole word, please try again.")
+        else:  # If input contains any numbers or special characters
+            print(Fore.RED + "Please try again with no numbers or special \
+                characters!")
 
 
 def main():
@@ -107,4 +136,5 @@ def main():
         word, hint = select_random_word()
         play_game(word, hint, username)
 
-main() # Runs main function for the game to play
+
+main()  # Runs main function for the game to play
