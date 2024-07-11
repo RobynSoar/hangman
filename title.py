b0f9639def003313bee667e0ad1b7497a91e2e61
Robyn Soar <robyn999@hotmail.co.uk>
2024-07-11 17:02:53 +0000

    Reduce spacing before title to negate title being pushed down

--- title.py
+++ title.py
@@ -1,4 +1,4 @@
 from pyfiglet import figlet_format
 from colorama import Fore, Style
 
-title = (Fore.YELLOW + figlet_format("   HANGMAN", font = "kban" ))
+title = (Fore.YELLOW + figlet_format(" HANGMAN", font = "kban" ))
